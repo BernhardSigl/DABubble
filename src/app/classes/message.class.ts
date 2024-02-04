1a6022361746fb03c042b779a8675bc1b28b6e31
Sanjaya Shrestha <102684222+pasa2002@users.noreply.github.com>
2024-02-04 16:15:47 +0100

    emoji reaction toggle

--- src/app/classes/message.class.ts
+++ src/app/classes/message.class.ts
@@ -1,43 +1,27 @@
-export class Message{
-  public messageId:string='';
+export class Message {
+  public messageId: string = '';
   public name: string = '';
   public time: number | string = '';
   public message: string[] = [];
-  public thread: string[] = []
-  public image : string = '';
+  public thread: string[] = [];
+  public image: string = '';
   public messageImage: string | File = '';
-  public senderId: string ='';
-  public reactions: { [key: string]: number } = {};
+  public senderId: string = '';
+  public reactions: { [key: string]: any } = {};
+  public users: { [key: string]: number } = {};
 
-
-  public addReaction(emoji: string) {
-    if (!this.reactions[emoji]) {
-      this.reactions[emoji] = 1;
-    } else {
-      this.reactions[emoji]++;
-    }
-  }
-
-  public removeReaction(emoji: string) {
-    if (this.reactions[emoji]) {
-      this.reactions[emoji]--;
-      if (this.reactions[emoji] === 0) {
-        delete this.reactions[emoji];
-      }
-    }
-  }
-
-  public toJson(){
-      return {
-          messageId: this.messageId,
-          name: this.name,
-          time: this.time,
-          message: this.message,
-          thread: this.thread,
-          image : this.image,
-          messageImage:this.messageImage,
-          senderId:this.senderId,
-          reactions: this.reactions
-      }
+  public toJson() {
+    return {
+      messageId: this.messageId,
+      name: this.name,
+      time: this.time,
+      message: this.message,
+      thread: this.thread,
+      image: this.image,
+      messageImage: this.messageImage,
+      senderId: this.senderId,
+      reactions: this.reactions,
+      users: this.users
+    };
   }
 }
--- src/app/main-chat/message-layout/message-layout.component.ts
+++ src/app/main-chat/message-layout/message-layout.component.ts
@@ -1,16 +1,18 @@
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MainChatComponent } from '../main-chat.component';
-import { Firestore, collection, query, orderBy, onSnapshot,doc, setDoc } from '@angular/fire/firestore';
+import { Firestore, collection, query, orderBy, onSnapshot, doc, setDoc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { Message } from '../../classes/message.class';
 import { CommonModule } from '@angular/common';
 import { getDownloadURL, getStorage, ref } from 'firebase/storage';
 import { PickerModule } from "@ctrl/ngx-emoji-mart";
+import { user } from '@angular/fire/auth';
+
 @Component({
   selector: 'app-message-layout',
   standalone: true,
-  imports: [MatDividerModule, MainChatComponent, CommonModule,PickerModule],
+  imports: [MatDividerModule, MainChatComponent, CommonModule, PickerModule],
   templateUrl: './message-layout.component.html',
   styleUrls: ['./message-layout.component.scss']
 })
@@ -26,9 +28,6 @@ export class MessageLayoutComponent implements OnInit {
   constructor(private firestore: Firestore) {}
   selectedMessage: Message | null = null;
 
-
-
-
   ngOnInit(): void {
     this.loadMessages();
   }
@@ -37,82 +36,136 @@ export class MessageLayoutComponent implements OnInit {
     const messagesCollection = collection(this.firestore, 'messages');
     const q = query(messagesCollection, orderBy('time', 'desc'));
     this.messages$ = new Observable<Message[]>(observer => {
-        onSnapshot(q, (querySnapshot) => {
-            const messages: Message[] = [];
-            querySnapshot.forEach(async doc => {
-                const messageData = doc.data() as Message;
-                // Assign the document ID as the messageId
-                messageData.messageId = doc.id; // Assuming doc.id is the ID of the document
-                if (messageData.messageImage) {
-                    const storage = getStorage();
-                    const imageRef = ref(storage, messageData.messageImage as string);
-                    messageData.messageImage = await getDownloadURL(imageRef);
-                }
-                messages.push(messageData);
-            });
-            observer.next(messages);
+      onSnapshot(q, (querySnapshot) => {
+        const messages: Message[] = [];
+        querySnapshot.forEach(async doc => {
+          const messageData = doc.data() as Message;
+          // Assign the document ID as the messageId
+          messageData.messageId = doc.id; // Assuming doc.id is the ID of the document
+          if (messageData.messageImage) {
+            const storage = getStorage();
+            const imageRef = ref(storage, messageData.messageImage as string);
+            messageData.messageImage = await getDownloadURL(imageRef);
+          }
+          messages.push(messageData);
         });
-    });
-}
-
-
-toggleEmojiPicker(messageId: string) {
-  if (!this.isEmojiPickerVisible[messageId]) {
-    // If emoji picker is not visible, close all other emoji pickers
-    Object.keys(this.isEmojiPickerVisible).forEach(key => {
-      this.isEmojiPickerVisible[key] = false;
+        observer.next(messages);
+      });
     });
   }
-  this.isEmojiPickerVisible[messageId] = !this.isEmojiPickerVisible[messageId];
-}
 
-onEmojiClick(event: any, message: Message) {
-  console.log("Selected emoji:", event.emoji.native);
-  this.addReaction(message, event.emoji.native);
-}
+  toggleEmojiPicker(messageId: string) {
+    if (!this.isEmojiPickerVisible[messageId]) {
+      // If emoji picker is not visible, close all other emoji pickers
+      Object.keys(this.isEmojiPickerVisible).forEach(key => {
+        this.isEmojiPickerVisible[key] = false;
+      });
+    }
+    this.isEmojiPickerVisible[messageId] = !this.isEmojiPickerVisible[messageId];
+  }
 
+  onEmojiClick(event: any, message: Message) {
+    console.log("Selected emoji:", event.emoji.native);
+    this.addReaction(message, event.emoji.native);
+  }
 
+  addReaction(message: Message, emoji: string) {
+    console.log("Adding reaction to message:", message);
+    console.log("Selected emoji:", emoji);
 
-addReaction(message: Message, emoji: string) {
-  console.log("Adding reaction to message:", message);
-  console.log("Selected emoji:", emoji);
-
-  if (!message.reactions) {
+    if (!message.reactions) {
       message.reactions = {}; // Ensure reactions object is initialized
-  }
+    }
 
-  if (!emoji) {
+    if (!emoji) {
       console.error("Selected emoji is undefined");
       return;
-  }
+    }
 
-  if (!message.reactions[emoji]) {
+    if (!message.reactions[emoji]) {
       message.reactions[emoji] = 1;
-  } else {
+    } else {
       message.reactions[emoji]++;
+    }
+
+    this.updateMessageReactions(message);
   }
 
-  this.updateMessageReactions(message);
-}
+  // toggleReaction(message: Message, emoji: string) {
+  //   if (!this.userId) {
+  //     console.error("UserID is not defined.");
+  //     return;
+  //   }
+
+  //   const userId = this.userId;
+
+  //   // Check if the user has already reacted to the emoji
+  //   if (message.reactions && message.reactions[emoji]) {
+  //     if (message.reactions[emoji] === 1 && message.senderId === userId) {
+  //       // If the count is 1 and the current user reacted, delete the reaction
+  //       delete message.reactions[emoji];
+  //     } else {
+  //       // Decrement the count if the current user reacted, or do nothing if another user reacted
+  //       message.reactions[emoji]--;
+  //     }
+  //   } else {
+  //     // If the emoji reaction doesn't exist, increment the count
+  //     if (!message.reactions) {
+  //       message.reactions = {}; // Ensure reactions object is initialized
+  //     }
+  //     message.reactions[emoji] = 1;
+  //   }
+
+  //   // Update reactions in Firestore
+  //   this.updateMessageReactions(message);
+  // }
+
+  toggleReaction(message: Message, emoji: string) {
+    if (!this.userId) {
+      console.error("UserID is not defined.");
+      return;
+    }
+
+    const userId = this.userId;
+
+    // Check if the user has already reacted to the emoji
+    if (message.reactions && message.reactions[emoji]) {
+      if (message.senderId === userId) {
+        // If the current user is the sender, decrement the count or delete the reaction
+        if (message.reactions[emoji] === 1) {
+          delete message.reactions[emoji]; // Delete the reaction if count is 1
+        } else {
+          message.reactions[emoji]--; // Decrement the count
+        }
+      } else {
+        // If the current user is not the sender, increment the count
+        message.reactions[emoji]++;
+      }
+    } else {
+      // If the emoji reaction doesn't exist, initialize it
+      if (!message.reactions) {
+        message.reactions = {}; // Ensure reactions object is initialized
+      }
+      // If the current user is not the sender, set the count to 1
+      message.reactions[emoji] = (message.senderId === userId) ? 1 : 1;
+    }
+
+    // Update reactions in Firestore
+    this.updateMessageReactions(message);
+  }
 
 
-updateMessageReactions(message: Message) {
-  console.log("Updating reactions in Firestore for message:", message);
+  updateMessageReactions(message: Message) {
+    // Update reactions in Firestore
+    const messageRef = doc(this.firestore, 'messages', message.messageId);
+    const reactionsData = message.reactions || {}; // Make sure the reaction object exists
 
-  const messageRef = doc(this.firestore, 'messages', message.messageId);
-  const reactionsData = message.reactions;
-
-  console.log("Reactions data:", reactionsData);
-
-  setDoc(messageRef, { reactions: reactionsData }, { merge: true })
-  // .then(() => {
-  //   console.log("Reactions updated successfully in Firestore.");
-  // })
-  // .catch(error => {
-  //   console.error("Error updating reactions in Firestore:", error);
-  // });
-}
-
-
-
+    setDoc(messageRef, { reactions: reactionsData }, { merge: true })
+    .then(() => {
+      console.log("Reactions successfully updated in Firestore.");
+    })
+    .catch(error => {
+      console.error("Error updating reactions in Firestore:", error);
+    });
+  }
 }
