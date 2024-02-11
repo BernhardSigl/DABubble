c3d0559fc58c300e79c00209ad8f5e9d4b443c94
Sanjaya Shrestha <102684222+pasa2002@users.noreply.github.com>
2024-02-11 16:05:06 +0100

    header changes copied from branch header side nav

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,6 +6,7 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { MatDialog } from '@angular/material/dialog';
 import { HeaderDropdownComponent } from '../popup/header-dropdown/header-dropdown.component';
+import { FirebaseService } from '../firebase-services/firebase.service';
 
 @Component({
   selector: 'app-header',
@@ -23,40 +24,27 @@ export class HeaderComponent implements OnInit {
   constructor(
     private firestore: Firestore,
     private route: ActivatedRoute,
+    public firebase: FirebaseService,
     public dialog: MatDialog) {
   }
 
-  ngOnInit() {
-    this.route.queryParams.subscribe(params=>{
-      this.userId = params['userId'];
-      // console.log(this.userId)
-      if (this.userId) {
-        this.getUserData(this.userId)
-      } else {
-        console.error('userId parameter us undefined')
-      }
-    })
+  // ngOnInit() {
+  //   this.route.queryParams.subscribe(params=>{
+  //     this.userId = params['userId'];
+  //     // console.log(this.userId)
+  //     if (this.userId) {
+  //       this.getUserData(this.userId)
+  //     } else {
+  //       console.error('userId parameter us undefined')
+  //     }
+  //   })
+  // }
+  async ngOnInit(): Promise<void> {
+    await this.firebase.ngOnInit();
   }
 
-  async getUserData(userId: string) {
-    try {
-      const usersCollection = collection(this.firestore, 'users');
-      const q = query(usersCollection, where('userId', '==', userId));
-      const querySnapshot = await getDocs(q);
-      querySnapshot.forEach(doc => {
-        const userData = doc.data();
-        this.userName = userData['name'];
-        this.userEmail = userData['email'];
-        this.userImage = userData['profileImg'];
 
-        // console.log(this.userName, this.userEmail, this.userImage)
-      })
 
-    }
-    catch (err) {
-      console.error(err)
-    }
-  }
 
   // async ngOnInit(): Promise<void> {
   //   await this.firebase.ngOnInit();
