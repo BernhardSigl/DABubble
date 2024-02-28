7d1d7e3246c8b22d5cdf5a2479e440a3c02a1ceb
Sanjaya Shrestha <102684222+pasa2002@users.noreply.github.com>
2024-02-28 14:34:00 +0100

    search function added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, NavigationEnd } from '@angular/router';
 import { AvatarDataService } from '../firebase-services/avatar-data.service';
 import { Firestore, addDoc, collection, getDocs, query, where } from '@angular/fire/firestore';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
@@ -7,11 +7,14 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { MatDialog } from '@angular/material/dialog';
 import { HeaderDropdownComponent } from '../popup/header-dropdown/header-dropdown.component';
 import { FirebaseService } from '../firebase-services/firebase.service';
-
+import { user } from '@angular/fire/auth';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { filter } from 'rxjs';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [AngularFirestoreModule],
+  imports: [AngularFirestoreModule,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
@@ -20,15 +23,28 @@ export class HeaderComponent implements OnInit {
   userEmail: string = '';
   userId: string = '';
   userImage: string = '';
-
+  channelArr:any='';
+  userArr:any='';
+  filteredChannels: any[] = [];
+  filteredUsers: any[] = [];
+  showDropdown: boolean = false;
+  searchQuery: string = '';
+  channelId:string='';
   constructor(
     public firebase: FirebaseService,
-    public dialog: MatDialog
-    ) {
+    public dialog: MatDialog,
+    public router: Router
+  ) {
+
   }
 
   async ngOnInit(): Promise<void> {
     await this.firebase.ngOnInit();
+    this.channelArr = this.firebase.channelsArray;
+    this.channelId= this.firebase.channelId;
+    this.userArr = this.firebase.usersArray;
+    this.filteredChannels = [...this.channelArr];
+    this.filteredUsers = [...this.userArr];
   }
 
   headerDropdownMenu() {
@@ -38,4 +54,36 @@ export class HeaderComponent implements OnInit {
     });
   }
 
+
+  search(event: Event) {
+    const query = (event.target as HTMLInputElement).value.trim();
+    const searchTerm = query.substring(1).toLowerCase();
+    this.searchQuery = query;
+  
+    if (query.startsWith('@')) {
+      this.filteredUsers = this.filterArray(this.userArr, 'name', searchTerm);
+      this.filteredChannels = []; // Reset channels
+      this.showDropdown = true;
+    } else if (query.startsWith('#')) {
+      this.filteredChannels = this.filterArray(this.channelArr, 'channelName', searchTerm);
+      this.filteredUsers = []; // Reset users
+      this.showDropdown = true;
+    } else {
+      this.showDropdown = false; // Hide dropdown if neither @ nor # is entered
+    }
+  }
+  
+  
+  filterArray(array: any[], propertyName: string, searchTerm: string): any[] {
+    return array.filter((item: any) => {
+      return item && item[propertyName] && item[propertyName].toLowerCase().includes(searchTerm);
+    });
+  }
+  
+  navigateToChannel(channelId: string) {
+    this.router.navigate(['/main', channelId]); 
+  }
+  navigateToUser(userId: string) {
+    this.router.navigate(['/private-chat', userId]); 
+  }
 }
