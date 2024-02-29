20bb4eccff2aa8db421a323b99801d5048e0a8d5
Sanjaya Shrestha <102684222+pasa2002@users.noreply.github.com>
2024-02-29 14:01:42 +0100

    From specific profile to private message fixed

--- src/app/popup/view-specific-profile/view-specific-profile.component.ts
+++ src/app/popup/view-specific-profile/view-specific-profile.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { FirebaseService } from '../../firebase-services/firebase.service';
 
 @Component({
   selector: 'app-view-specific-profile',
@@ -17,30 +18,35 @@ export class ViewSpecificProfileComponent {
   profileImg!: string;
   // name!: string;
   // status!: boolean;
-  // userId!: string;
+  userId: string='';
 
 
 constructor(
   @Inject(MAT_DIALOG_DATA) public data: any,
   public dialogRef: MatDialogRef<ViewSpecificProfileComponent>,
-  private router:Router
+  private router:Router,
+  private firebase:FirebaseService
 ) {
 
 // this.name = this.data.name;
 // this.status = this.data.status
 }
 
-routeToPrivateChat(){
+async routeToPrivateChat(userId:string){
   console.log(this.data.user.userId);
-  const userId = this.data.user.userId;
+  const user = this.firebase.usersArray.find(
+    (user)=>user.userId === userId
+  )
 
-  if (userId) {
-    // Navigate to the private chat route with the userId
-    this.router.navigate(['/private-chat', userId]);
-  } else {
-    console.error('User ID not found.');
-  }
+  this.firebase.setSelectedChannelId(userId);
+  await this.firebase.activeChannelId(
+    'privateChat',
+    `${userId}_${this.firebase.loggedInUserId}`
+  );
+  await this.firebase.channelOrPrivateChat('privateChat');
+  await this.firebase.addNewPrivateMessage(user);
 }
 }
+
 
 
