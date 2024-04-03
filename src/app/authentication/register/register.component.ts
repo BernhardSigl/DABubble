24770ead0867e8841831b2a0d9bc739847c31bc8
Bernhard Sigl <b-sigl@live.de>
2024-04-03 16:23:34 +0200

    register: email must be valid

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,17 +1,14 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
-import { AuthyService } from '../../firebase-services/authy.service';
-import { User, AppUser } from '../../classes/user.class';
 import { AvatarDataService } from '../../firebase-services/avatar-data.service';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { PolicyPopupComponent } from '../../popup/policy-popup/policy-popup.component';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { ImprintPopupComponent } from '../../popup/imprint-popup/imprint-popup.component';
 @Component({
   selector: 'app-register',
@@ -28,21 +25,18 @@ import { ImprintPopupComponent } from '../../popup/imprint-popup/imprint-popup.c
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
-  // fireStore:AngularFirestore = inject(AngularFirestore)
   checkboxChecked: boolean = false;
 
   loginForm = new FormGroup({
     name: new FormControl('', Validators.required),
-    email: new FormControl('', [Validators.required, Validators.email]),
+    email: new FormControl('', [Validators.required, Validators.email, validateEmail]),
     password: new FormControl('', Validators.required),
   });
 
   constructor(
     private router: Router,
-    private authyService: AuthyService,
     private avatarDataService: AvatarDataService,
     public dialog: MatDialog,
-    // public dialogRef: MatDialogRef<RegisterComponent>,
   ) {
     this.avatarDataService.selectedAvatar$.subscribe((avatarSrc) => { });
   }
@@ -76,4 +70,16 @@ export class RegisterComponent {
       panelClass: ['border', 'imprint-policy-popup']
     });
   }
+}
+
+function validateEmail(control: AbstractControl): { [key: string]: any } | null {
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if (control.value && !emailPattern.test(control.value)) {
+    return { 'email': true };
+  }
+  const dotIndex = control.value.lastIndexOf('.');
+  if (dotIndex === -1 || dotIndex === control.value.length - 1) {
+    return { 'invalidEnd': true };
+  }
+  return null;
 }
